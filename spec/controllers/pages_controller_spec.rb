5b739a6be92461202aa46c39866436b163123d26
JT Mitchum <jtmitchum@gmail.com>
2011-03-12 10:08:48 -0600

    Added style sheets, title helper

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,11 +1,18 @@
 require 'spec_helper'
 
+
 describe PagesController do
+render_views
 
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
+    end
+    
+    it "should have the right title" do
+      get 'home'
+      response.should have_selector("title", :content =>"RightBy.US | Home |")
     end
   end
 
@@ -14,12 +21,22 @@ describe PagesController do
       get 'about'
       response.should be_success
     end
+    
+    it "should have the right title" do
+      get 'about'
+      response.should have_selector("title", :content =>"RightBy.US | About |")
+    end
   end
 
   describe "GET 'contact'" do
     it "should be successful" do
       get 'contact'
       response.should be_success
+    end
+    
+    it "should have the right title" do
+      get 'contact'
+      response.should have_selector("title", :content =>"RightBy.US | Contact |")
     end
   end
 
